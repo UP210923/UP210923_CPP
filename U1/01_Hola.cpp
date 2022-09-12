f72e9a8b2abbaf9ee94990f23377a8f2d627e887
UP210739 <up210739@alumnos.upa.edu.mx>
2022-09-12 10:54:02 -0500

    Primer trabajo

---
+++ U1/01_Hola.cpp
@@ -0,0 +1,23 @@
+/*
+Date: 12/09/2022
+Author: Alan Alejandro Hernandez Macias
+Email: up210923@alumnos.upa.edu.mx
+Description: Mi primer programa en c++
+Last Modification:
+*/
+
+//------------ PREPROCESSOR DIRECTIVES ----------------
+//Input / Output Library between the screem and keyboard
+
+#include <iostream> 
+
+//------- MAIN FUNCTION ---------
+
+int main(){
+    //Prints on the screen a text 
+    std::cout <<"Hello World! This is my first program";
+
+    // Return 0 means that the programm ends succesfully
+    return 0;
+
+}
