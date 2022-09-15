14e0b8b90711fa06f1852d92094c857ec3a8656b
UP210739 <up210739@alumnos.upa.edu.mx>
2022-09-15 10:00:16 -0500

    Actualizando

--- U1/01_Hola.cpp
+++ U1/01_Hola.cpp
@@ -7,17 +7,14 @@ Last Modification:
 */
 
 //------------ PREPROCESSOR DIRECTIVES ----------------
-//Input / Output Library between the screem and keyboard
-
+//Input / Output Library between the screen and keyboard
 #include <iostream> 
 
 //------- MAIN FUNCTION ---------
-
 int main(){
     //Prints on the screen a text 
     std::cout <<"Hello World! This is my first program";
 
     // Return 0 means that the programm ends succesfully
     return 0;
-
 }
