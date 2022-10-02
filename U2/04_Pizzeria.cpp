9e4cc060b8f62bc9dbbe48c4539d676fc80e9fba
UP210923 <up210923@alumnos.upa.edu.mx>
2022-10-02 18:52:43 -0500

    Pizza updated

--- U2/04_Pizzeria.cpp
+++ U2/04_Pizzeria.cpp
@@ -5,8 +5,7 @@ Email: up210923@alumnos.upa.edu.mx
 Description: i gonna talk about a pizzeria where there u can select
 multiple ingredients
 Last Modification:
-Escribir un programa que pregunte al usuario si quiere una pizza
-vegetariana o no
+ Write a program that ask the user if he wants a vegan pizza or not 
 */
 #include <iostream>
 
@@ -16,50 +15,50 @@ int main()
 {
     char pizza;
     int ingrediente;
-    cout << "¿Quieres una pizza vegetariana? RECUERDA todas nuestras pizzas llevan mozzarella y tomate\n Y/N \n";
+    cout << "¿Do you want a vegetarian pizza ? REMEMBER all our pizza have mozzarella and tomate\n Y/N \n";
     cin >> pizza;
 
     if (pizza == 'Y' || pizza == 'y')
     {
-        cout << " Estos son tus ingredientes para tu pizza vegetariana \n¿Cual desea agregar? : \n 1.Pimiento \n 2.Tofu \n Escoge un ingrediente extra para agregar a tu pizza: ";
+        cout << " These are your ingredients for your vegan pizza \n¿Which toppings do you like to add? : \n 1.Pepper \n 2.Tofu \n Pick an extra topping to add to the pizza: ";
         cin >> ingrediente;
 
-         cout << "Tu pizza es vegetariana, tus ingredientes son mozarrella y tomate:" << endl;
+         cout << "Your pizza is vegetarian, Your ingredients are mozarrella and tomate:" << endl;
 
         if (ingrediente == 1)
         {
-            cout << "-Tu ingrediente extra para tu pizza sera: Pimiento " << endl;
+            cout << "-Your extra ingredient for your pizza will be: Pepper " << endl;
         }
         else if (ingrediente == 2)
         {
-            cout << "-Tu ingrediente extra para tu pizza sera: Tofu " << endl;
+            cout << "-Your extra ingredient for your pizza will be: Tofu " << endl;
         }
 
        
     }
     else if (pizza == 'N' || pizza == 'n')
     {
-        cout << "Estos son tus otros ingredientes para tu pizza no vegetariana \n¿Cual desea agregar?: \n 1.Peperoni \n 2.Jamon \n 3.Salmon \n Escoge un ingrediente extra para agregar a tu pizza: ";
+        cout << "These are your ingredients for your no vegan pizza \n¿Which toppings do you like to add?: \n 1.Pepperoni \n 2.Ham \n 3.Salmon \n Pick an extra topping to add to the pizza: ";
         cin >> ingrediente;
 
-         cout << "Tu pizza no es vegetariana, tus ingredientes son mozarrella y tomate: " << endl;
+         cout << "Your pizza is not vegetarian, Your ingredients are mozarrella and tomate: " << endl;
 
         if (ingrediente == 1)
         {
-            cout << "-Tu ingrediente extra para tu pizza sera: Peperoni";
+            cout << "-Your extra ingredient for your pizza will be: Pepperoni";
         }
         else if (ingrediente == 2)
         {
-            cout << "-Tu ingrediente extra para tu pizza sera: Jamon";
+            cout << "-Your extra ingredient for your pizza will be: Ham";
         }
         else if (ingrediente == 3)
         {
-            cout << "-Tu ingrediente extra para tu pizza sera: Salmon";
+            cout << "-Your extra ingredient for your pizza will be: Salmon";
         }
     }
     else
     {
-        cout << "Solo se permite Y (es si) o N (es no)";
+        cout << "Just allowed Y (its yes) or N (its no)";
     }
 
    
