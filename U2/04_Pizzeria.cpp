8dcdf9f4f5074ff9abcc1de6518e375ad007de16
UP210923 <up210923@alumnos.upa.edu.mx>
2022-10-02 18:54:01 -0500

    Pizza updated translated}

--- U2/04_Pizzeria.cpp
+++ U2/04_Pizzeria.cpp
@@ -15,7 +15,7 @@ int main()
 {
     char pizza;
     int ingrediente;
-    cout << "¿Do you want a vegetarian pizza ? REMEMBER all our pizza have mozzarella and tomate\n Y/N \n";
+    cout << "¿Do you want a vegetarian pizza ? REMEMBER all our pizza have mozzarella and tomato\n Y/N \n";
     cin >> pizza;
 
     if (pizza == 'Y' || pizza == 'y')
@@ -23,7 +23,7 @@ int main()
         cout << " These are your ingredients for your vegan pizza \n¿Which toppings do you like to add? : \n 1.Pepper \n 2.Tofu \n Pick an extra topping to add to the pizza: ";
         cin >> ingrediente;
 
-         cout << "Your pizza is vegetarian, Your ingredients are mozarrella and tomate:" << endl;
+         cout << "Your pizza is vegetarian, Your ingredients are mozarrella and tomato:" << endl;
 
         if (ingrediente == 1)
         {
@@ -41,7 +41,7 @@ int main()
         cout << "These are your ingredients for your no vegan pizza \n¿Which toppings do you like to add?: \n 1.Pepperoni \n 2.Ham \n 3.Salmon \n Pick an extra topping to add to the pizza: ";
         cin >> ingrediente;
 
-         cout << "Your pizza is not vegetarian, Your ingredients are mozarrella and tomate: " << endl;
+         cout << "Your pizza is not vegetarian, Your ingredients are mozarrella and tomato: " << endl;
 
         if (ingrediente == 1)
         {
