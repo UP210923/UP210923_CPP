e563f8af37fffdc4650df849514a409f991fc3f7
UP210923 <up210923@alumnos.upa.edu.mx>
2022-10-02 19:54:45 -0500

    Already

--- U2/01_Renta.cpp
+++ U2/01_Renta.cpp
@@ -3,7 +3,7 @@ Date: 22/09/2022
 Author: Alan Alejandro Hernandez Macias
 Email: up210923@alumnos.upa.edu.mx
 Description:
-Los tramos inpositivos para la declaracion de la renta de un determinado pais
+The tax brackets for the declaration of income of a certain country
 
 Last Modification:
 */
@@ -13,9 +13,26 @@ Last Modification:
 using namespace std;
 
     int main(){
-        int renta;
-        int descuento;
-        int TotalPago
+        double renta;
 
-        cout << "Cuanto es tu renta carnal: ";
+        cout << "How much is your rent? carnal: ";
+        cin >> renta;
+
+        if (renta < 10000) {
+            cout <<"you have to pay $" <<renta*.05;
+            cout << " in taxes";
+        }
+        else if (renta >= 10000 && renta < 20000) {
+            cout << "you have to pay $" <<renta*.15;
+            cout << " in taxes";
+        }
+        else if (renta >= 20000 && renta < 35000) {
+            cout << "you have to pay $" <<renta*.20;
+            cout << " in taxes";
+        }
+        else if (renta >= 35000 && renta < 60000) {
+            cout << "you have to pay $" <<renta*.30;
+            cout << " in taxes";
+        }
+        return 0;
     }
