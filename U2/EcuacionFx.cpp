40fa34938228c282d1736b4b22ba77fe9b3a3149
UP210923 <Up210923@alumnos.upa.edu.mx>
2022-10-13 10:40:04 -0500

    Actualizando

---
+++ U2/EcuacionFx.cpp
@@ -0,0 +1,17 @@
+/*Date: 12/10/2022
+Author: Alan Alejandro Hernandez Macias
+Email: up210923@alumnos.upa.edu.mx
+Description: i gonna talk about a pizzeria where there u can select
+multiple ingredients
+Last Modification:
+ Write a program that ask the user if he wants a vegan pizza or not
+*/
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+float resolverEcuacion(float valor){
+    //con la ecuacion de x^2+3x+8
+    return pow(valor, 2)+ 3 *valor + 8;
+}
+int ejem
