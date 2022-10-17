4167832a97c0f5fa3d99e6611f695e603539bd5a
UP210923 <Up210923@alumnos.upa.edu.mx>
2022-10-17 10:35:02 -0500

    AHi vamos

--- U2/06_Products.cpp
+++ U2/06_Products.cpp
@@ -10,27 +10,24 @@ using namespace std;
 
 int main()
 {
-	float precio, cantiProduct = 0, total, suma;
-	cout << "Bienvenidos a su tienda \t  ." << " en donde debe ingresar el nombre de su producto con cantidad y su precio";
+	float precio, cantiProduct = 0, total=0, suma;
+	cout << "Bienvenidos a su tienda \t \n "
+		 << "En donde debe ingresar el nombre de su producto con cantidad y su precio";
 	do
 	{
 		cout << "\n Cuantos productos necesita? ";
 		cin >> cantiProduct;
-		if (cantiProduct < 0);
+		if (cantiProduct < 0)
 		{
-		cout << "Necesito una cantidad real de producto: ";
-		break;
+			cout << "Necesito una cantidad real de producto: ";
+			break;
 		}
-	cout << "De cuanto es el precio del producto: ";
-	cin >> precio;
-	suma = cantiProduct*precio;
-	total += suma;
-	
-	while (cantiProduct != 0);
-	{
-		cout << "Tu total de productos son : " << total << "y su precio sera" << precio;
+		cout << "De cuanto es el precio del producto $ : ";
+		cin >> precio;
+		suma = cantiProduct * precio;
+		total += suma;
 
-		return 0;
-
-
-	}
+	}while (cantiProduct != 0);
+			cout << "El total de tu factura sera:$" << total<<endl;
+			return 0;
+		}
