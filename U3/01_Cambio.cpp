cbc8c8a13e44f009fb5662336020f361750f2d02
UP210923 <Up210923@alumnos.upa.edu.mx>
2022-10-24 10:40:15 -0500

    Ahi intentandolo

---
+++ U3/01_Cambio.cpp
@@ -0,0 +1,19 @@
+/*
+Author: Alan Alejandro Hernandez Macias
+Date: 24/10/2022
+Description: 
+*/
+#include <iostream>
+
+using namespace std;
+
+void obtenertotal(int);
+void obtenermonedas(int);
+void imprimircambio(string);
+
+int quinientos = 0, doscientos = 0, cien = 0;
+int cincuenta = 0, veinte = 0;
+int diez = 0 , cinco = 0, dos = 0, uno = 0;
+
+void obtenertotal (int);
+
