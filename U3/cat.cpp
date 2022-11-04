f6c5010200365bf44fdd3241164367ba5f501fde
UP210923 <Up210923@alumnos.upa.edu.mx>
2022-11-04 09:48:59 -0600

    WOkring

---
+++ U3/cat.cpp
@@ -0,0 +1,19 @@
+/*
+Author: Alan Alejandro Hernandez Macias
+Date: 03/11/2022
+Description: 
+*/
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    cout << "    |     |    \n  "\033[0m";
+    cout << "____|_____|____\n  "\033[0m";
+    cout << "    |     |    \n  "\033[0m";
+    cout << "    |     |    \n  "\033[0m";
+    cout << "____|_____|____\n  "\033     ;
+    cout << "    |     |    \n";
+    cout << "    |     |    \n";
+    cout << "\033[0;31m" << "SI" << "\033[0m";
+}
